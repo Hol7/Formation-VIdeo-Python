a3c80fe1be5c1caa265a9778f44f3157a5924bc7
Fabrone BITITI <40841286+Hol7@users.noreply.github.com>
2021-09-05 18:00:16 +0000

    Create fonction.py

---
+++ fonction.py
@@ -0,0 +1,12 @@
+# definir une fontion et appellé la fonction
+
+# omment definir une fonction
+
+def mbote():
+    print("bonjour Moninga")
+
+def loba(nom_personne, nationalité):
+  print("je m'appelle {} je suis {} ".format(nom_personne, nationalité ))
+
+mbote()
+loba("fabrone", 'congolais')
